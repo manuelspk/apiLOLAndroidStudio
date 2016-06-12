ccfd33e10da76fb0cd91db60a2a0f39810cfaa8c
Manuel <manuel.spk@gmail.com>
2016-06-12 20:23:51 +0200

    Más estructurado

--- app/src/main/java/com/example/manana/apilol/MainActivity.java
+++ app/src/main/java/com/example/manana/apilol/MainActivity.java
@@ -30,10 +30,6 @@ public class MainActivity extends AppCompatActivity {
     private TextView lblNivel;
     private TextView lblIcono;
 
-    static final String API_KEY = "4ab45b6d-89ca-4679-aaaa-95c75c00a6c5";
-    static final String API_URL = "https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/";
-
-    Usuario usuario = new Usuario();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,73 +48,12 @@ public class MainActivity extends AppCompatActivity {
         queryButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                usuario.summonerName=emailText.getText().toString();
-                new RetrieveFeedTask().execute();
+
+                Usuario usuario = new Usuario(lblNombre,lblNivel,lblIcono, "manuelspk");
+                usuario.execute(emailText.getText().toString());
             }
         });
     }
 
-    class RetrieveFeedTask extends AsyncTask<Void, Void, String> {
 
-        private Exception exception;
-
-        protected void onPreExecute() {
-            progressBar.setVisibility(View.VISIBLE);
-
-        }
-
-        protected String doInBackground(Void... urls) {
-
-            // Do some validation here
-
-            try {
-                URL url = new URL(API_URL + usuario.summonerName + "?api_key=" + API_KEY);
-                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-                try {
-                    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
-                    StringBuilder stringBuilder = new StringBuilder();
-                    String line;
-                    while ((line = bufferedReader.readLine()) != null) {
-                        stringBuilder.append(line).append("\n");
-                    }
-                    bufferedReader.close();
-                    return stringBuilder.toString();
-                }
-                finally{
-                    urlConnection.disconnect();
-                }
-            }
-            catch(Exception e) {
-                Log.e("ERROR", e.getMessage(), e);
-                return null;
-            }
-        }
-
-        protected void onPostExecute(String response) {
-            if(response == null) {
-                response = "THERE WAS AN ERROR";
-            }
-            progressBar.setVisibility(View.GONE);
-            Log.i("INFO", response);
-
-            // TODO: check this.exception
-            // TODO: do something with the feed
-
-            try {
-            JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
-                usuario.name = object.getJSONObject(String.valueOf(usuario.summonerName)).getString("name");
-                usuario.id=object.getJSONObject(String.valueOf((usuario.summonerName))).getLong("id");
-                usuario.profileIconId=object.getJSONObject(String.valueOf((usuario.summonerName))).getInt("profileIconId");
-                usuario.revisionDate=object.getJSONObject(String.valueOf((usuario.summonerName))).getLong("revisionDate");
-                usuario.summonerLevel=object.getJSONObject(String.valueOf((usuario.summonerName))).getLong("summonerLevel");
-
-                lblNombre.setText(usuario.name);
-                lblNivel.setText(String.valueOf(usuario.summonerLevel));
-                lblIcono.setText(String.valueOf(usuario.profileIconId));
-
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 }
--- app/src/main/java/com/example/manana/apilol/Usuario.java
+++ app/src/main/java/com/example/manana/apilol/Usuario.java
@@ -1,9 +1,27 @@
 package com.example.manana.apilol;
 
+import android.os.AsyncTask;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 /**
  * Created by manana on 7/6/16.
  */
-public class Usuario {
+public class Usuario extends AsyncTask<String, Void, String>{
+
+    private TextView lblNombre;
+    private TextView lblNivel;
+    private TextView lblIcono;
 
     long id;
     String summonerName;
@@ -12,6 +30,71 @@ public class Usuario {
     long revisionDate;
     long summonerLevel;
 
+    static final String API_KEY = "4ab45b6d-89ca-4679-aaaa-95c75c00a6c5";
+    static final String API_URL = "https://euw.api.pvp.net/api/lol/euw/v1.4/summoner/by-name/";
+
+    public Usuario(TextView lblNombre, TextView lblNivel, TextView lblIcono, String Invocador)
+    {
+        this.lblNombre=lblNombre;
+        this.lblNivel=lblNivel;
+        this.lblIcono=lblIcono;
+        this.summonerName=Invocador;
+    }
 
 
+
+    @Override
+    protected String doInBackground(String... params) {
+        try {
+            URL url = new URL(API_URL + params[0] + "?api_key=" + API_KEY);
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            try {
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line).append("\n");
+                }
+                bufferedReader.close();
+                return stringBuilder.toString();
+            }
+            finally{
+                urlConnection.disconnect();
+            }
+        }
+        catch(Exception e) {
+            Log.e("ERROR", e.getMessage(), e);
+            return null;
+        }
+    }
+
+
+    @Override
+
+
+    protected void onPostExecute(String response) {
+        if(response == null) {
+            response = "THERE WAS AN ERROR";
+        }
+        Log.i("INFO", response);
+
+        // TODO: check this.exception
+        // TODO: do something with the feed
+
+        try {
+            JSONObject object = (JSONObject) new JSONTokener(response).nextValue();
+            this.name = object.getJSONObject(String.valueOf(this.summonerName)).getString("name");
+            this.id=object.getJSONObject(String.valueOf((this.summonerName))).getLong("id");
+            this.profileIconId=object.getJSONObject(String.valueOf((this.summonerName))).getInt("profileIconId");
+            this.revisionDate=object.getJSONObject(String.valueOf((this.summonerName))).getLong("revisionDate");
+            this.summonerLevel=object.getJSONObject(String.valueOf((this.summonerName))).getLong("summonerLevel");
+
+            lblNombre.setText(this.name);
+            lblNivel.setText(String.valueOf(this.summonerLevel));
+            lblIcono.setText(String.valueOf(this.profileIconId));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
